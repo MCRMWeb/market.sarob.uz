c5364e13bbe34eee9c72afc0190379791be60561
Rafael <rafa92muhitdinov@gmail.com>
2024-08-15 15:43:14 +0500

    save

---
+++ assets/Cart-CZIeJ67D.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-7WS4nBfv.js";function a(){return r.jsx("div",{children:"Cart"})}export{a as default};
--- templates/online-store/ThemDefault.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ThemDefault() {
-  return (
-    <div>ThemDefault</div>
-  )
-}
